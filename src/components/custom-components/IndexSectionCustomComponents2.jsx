54a93da5b4ea457b119f898e326fb34840daa297
theyodaknows <168790738+theyodaknows@users.noreply.github.com>
2025-08-16 12:54:09 -0700

    Add Facebook icon to footer

--- src/components/custom-components/IndexSectionCustomComponents2.jsx
+++ src/components/custom-components/IndexSectionCustomComponents2.jsx
@@ -474,6 +474,23 @@ const IndexSectionCustomComponents2 = () => {
             </div>
           </div>
 
+          {/* Social Media Links */}
+          <div className="border-t border-gray-700 pt-8 mb-8 text-center">
+            <div className="flex justify-center space-x-6">
+              <a 
+                href="https://www.facebook.com/justbookandgola/" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                className="text-gray-400 hover:text-blue-500 transition-colors"
+                aria-label="Follow us on Facebook"
+              >
+                <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                  <path fillRule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clipRule="evenodd" />
+                </svg>
+              </a>
+            </div>
+          </div>
+
           <div className="border-t border-gray-700 pt-8 text-center text-gray-400">
             <p>© 2024 Justbookandgo. All rights reserved. | Licensed, Bonded & Insured in Los Angeles County</p>
           </div>
